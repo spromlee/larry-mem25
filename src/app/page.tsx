a6afa852c2e2c11cf347b5452acc44ca4b37a86a
ThinkerSajjad <muhammad.sajjad.pstu@gmail.com>
2025-02-09 02:36:50 +0600

    fix: linting errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+
 import Header from "@/components/Header";
 import Navigation from "@/components/Navigation";
 import Obituary from "@/components/sections/Obituary";
